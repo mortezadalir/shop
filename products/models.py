18dc8019e8f8cbc7ce496c113bde94a22218934e
morteza dalirian <morteza.daliriyan@gmail.com>
2020-12-27 12:37:31 +0330

    delete

--- products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Products(models.Model):
-    name_prod=models.CharField(max_length=256)
-    type_prod=models.CharField(max_length=256)
-    price_prod=models.FloatField()
-    
-
